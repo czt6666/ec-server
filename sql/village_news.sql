f1888b56d8e71182ef792816aa8be938b4a342e3
zgm0123 <1450060957@qq.com>
2025-11-16 12:58:14 +0800

    sql fix

--- sql/village_news.sql
+++ sql/village_news.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80043 (8.0.43)
  File Encoding         : 65001
 
- Date: 29/10/2025 20:15:09
+ Date: 16/11/2025 12:57:46
 */
 
 SET NAMES utf8mb4;
@@ -25,7 +25,7 @@ CREATE TABLE `village_news` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL COMMENT '新闻标题',
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '编写用户',
-  `village_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '隶属乡村ID',
+  `village_id` int NOT NULL COMMENT '隶属乡村ID',
   `image_url` varchar(1024) DEFAULT NULL COMMENT '封面图片URL（单张）',
   `content` mediumtext NOT NULL COMMENT '新闻内容',
   `published_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
@@ -35,9 +35,9 @@ CREATE TABLE `village_news` (
   `publish_status` tinyint NOT NULL DEFAULT '1' COMMENT '发布状态：1-草稿 2-已发布 3-已下线',
   PRIMARY KEY (`id`),
   KEY `idx_author_id` (`author`),
-  KEY `idx_village_id` (`village_name`),
+  KEY `idx_village_id` (`village_id`),
   KEY `idx_published_at` (`published_time`),
-  CONSTRAINT `village` FOREIGN KEY (`village_name`) REFERENCES `village` (`village_name`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='新闻表';
+  CONSTRAINT `villageid` FOREIGN KEY (`village_id`) REFERENCES `village` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=127 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='新闻表';
 
 SET FOREIGN_KEY_CHECKS = 1;
