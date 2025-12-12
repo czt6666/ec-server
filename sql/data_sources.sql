c552a5bf413d6a65a184b0ee8d5f08d95e3b596b
zgm0123 <1450060957@qq.com>
2025-12-12 21:19:13 +0800

    add uploads

--- sql/data_sources.sql
+++ sql/data_sources.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80043 (8.0.43)
  File Encoding         : 65001
 
- Date: 12/12/2025 00:57:42
+ Date: 12/12/2025 21:17:08
 */
 
 SET NAMES utf8mb4;
@@ -26,12 +26,12 @@ CREATE TABLE `data_sources` (
   `name` varchar(100) NOT NULL COMMENT '业务模块名称',
   `table_name` varchar(100) NOT NULL COMMENT '对应的数据表名',
   `status` tinyint DEFAULT '1' COMMENT '状态(0-禁用, 1-启用)',
-  `sync_frequency` varchar(50) DEFAULT '0 0 * * * ?' COMMENT '同步频率(cron表达式)',
   `last_sync_time` datetime DEFAULT NULL COMMENT '最后同步时间',
   `record_count` int DEFAULT '0' COMMENT '记录数',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sync_frequency` varchar(50) DEFAULT '0 0 * * * ?' COMMENT '同步频率(cron表达式)',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
